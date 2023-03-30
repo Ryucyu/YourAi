caab28ef5303cc46cde5c86fc3125b3a27efbccc
chenyuu.liu <chennyu.liu@dbappsecurity.com.cn>
2023-03-30 14:43:01 +0800

    前后端合二为一

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.YourAi.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YourAi.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
